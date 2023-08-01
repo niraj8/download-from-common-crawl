f63bd0bdee47a5d3ee617a76d15a10bf45a9e79c
Niraj Palecha <nirajftw@gmail.com>
2023-08-01 22:42:11 +0530

    simplify main(), use warcio to parse the warc from stream

--- python/main.py
+++ python/main.py
@@ -1,16 +1,17 @@
-import os
 import boto3
 
+from io import BytesIO
 from athena import AthenaQuery, Athena
 from commoncrawl import CommonCrawlQuery
 from constants import S3_RESULTS_BUCKET_NAME, AWS_REGION
 
-from query_results import get_uncompressed_warc_file_content_from_s3_key, query_results
+from query_results import get_warc, query_results
 
 
 def main():
     print("Using S3 bucket for results:", S3_RESULTS_BUCKET_NAME)
     athena = Athena(boto3.client("athena", region_name=AWS_REGION))
+    s3_client = boto3.client("s3")
 
     query = AthenaQuery(
         CommonCrawlQuery(
@@ -22,24 +23,15 @@ def main():
         )
     )
 
-    output_location = "s3://{}/results".format(S3_RESULTS_BUCKET_NAME)
-    query_execution_id = athena.execute_query(query.get_query_string(), output_location)
-    print("Athena query execution id:", query_execution_id)
-
-    query_status = athena.poll_query_execution_status(query_execution_id)
-    if query_status["State"] == "FAILED" or query_status["State"] == "CANCELLED":
-        raise RuntimeError("Query failed or cancelled:", query_status)
-
-    s3_client = boto3.client("s3")
-    results = query_results(s3_client, S3_RESULTS_BUCKET_NAME, query_execution_id)
+    results = query_results(athena, s3_client, query)
     for row in results:
-        if len(row) == 3:
-            print(row)
-            warc_file_content = get_uncompressed_warc_file_content_from_s3_key(
-                s3_client, row[0], int(row[1]), int(row[2])
-            )
-            # TODO do something with warc_file_content
-            print(warc_file_content)
+        print("start processing:", row)
+        warc_object = get_warc(s3_client, row[0], int(row[1]), int(row[2]))
+        # TODO do something with warc_object
+
+        print(warc_object)
+        print(warc_object.rec_headers)
+        print(warc_object.content_stream().read())
 
 
 main()
--- python/query_results.py
+++ python/query_results.py
@@ -1,11 +1,22 @@
 import csv
-import gzip
-import io
+from io import BytesIO
+from warcio.archiveiterator import WARCIterator
+
+from constants import S3_RESULTS_BUCKET_NAME
 
 
-def query_results(s3_client, s3_results_bucket_name, query_execution_id):
+def query_results(athena, s3_client, query):
+    output_location = "s3://{}/results".format(S3_RESULTS_BUCKET_NAME)
+
+    query_execution_id = athena.execute_query(query.get_query_string(), output_location)
+    print("Athena query execution id:", query_execution_id)
+
+    query_status = athena.poll_query_execution_status(query_execution_id)
+    if query_status["State"] == "FAILED" or query_status["State"] == "CANCELLED":
+        raise RuntimeError("Query failed or cancelled:", query_status)
+
     response = s3_client.get_object(
-        Bucket=s3_results_bucket_name,
+        Bucket=S3_RESULTS_BUCKET_NAME,
         Key="results/" + query_execution_id + ".csv",
     )
 
@@ -15,14 +26,17 @@ def query_results(s3_client, s3_results_bucket_name, query_execution_id):
         # parse csv from results_content
         csv_reader = csv.reader(results_content.split("\n"))
         next(csv_reader)  # skip header
+
+        # skip empty rows
+        for row in csv_reader:
+            if len(row) > 0:
+                yield row
         return csv_reader
     else:
         raise RuntimeError("Failed to get results from s3", response)
 
 
-def get_uncompressed_warc_file_content_from_s3_key(
-    s3_client, s3_key, warc_offset, warc_length
-):
+def get_warc(s3_client, s3_key, warc_offset, warc_length):
     response = s3_client.get_object(
         Bucket="commoncrawl",
         Key=s3_key,
@@ -32,5 +46,6 @@ def get_uncompressed_warc_file_content_from_s3_key(
     if response["ResponseMetadata"]["HTTPStatusCode"] != 206:
         raise RuntimeError("Failed to get warc file from s3", response)
 
-    content = response["Body"].read()
-    return gzip.GzipFile(fileobj=io.BytesIO(content)).read()
+    # only 1 record in our warc files
+    for record in WARCIterator(BytesIO(response["Body"].read())):
+        return record
